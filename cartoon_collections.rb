4ac7c31d57d480387823959557e5010de61845d1
José Romero <jdromerov88@gmail.com>
2019-10-26 05:09:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  while i=0 i<array.length do
+    puts array[i]
+    i+=1
+  end
 end
 
 def summon_captain_planet# code an argument here
